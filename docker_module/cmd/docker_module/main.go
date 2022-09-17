5c1d9aa301c068cb506bb7562cc7ba753834de25
ginalig <aanigay@edu.hse.ru>
2022-09-17 12:50:12 +0300

    Hui

--- docker_module/cmd/docker_module/main.go
+++ docker_module/cmd/docker_module/main.go
@@ -1,25 +1,11 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	cli, err := client.NewClientWithOpts(client.FromEnv)
-	if err != nil {
-		panic(err)
-	}
+	router := gin.Default()
 
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	for _, container := range containers {
-		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
-	}
+	router.Run("localhost:8080")
 }
