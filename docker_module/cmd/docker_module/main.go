18af0bf9a53038664e38ca32ff1c551af489f165
ginalig <aanigay@edu.hse.ru>
2022-09-17 13:07:49 +0300

    Added GET

--- docker_module/cmd/docker_module/main.go
+++ docker_module/cmd/docker_module/main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
 	router := gin.Default()
 
-	router.Run("localhost:8080")
+	router.GET(":message", get)
+
+	err := router.Run("localhost:8080")
+	if err != nil {
+		return
+	}
+}
+
+func get(c *gin.Context) {
+	message := c.Param("message")
+	c.String(http.StatusOK, message)
 }
